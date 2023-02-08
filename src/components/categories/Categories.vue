d8c4b84f2580642a98a4235bb01a1965c6bade73
RandyAndrian <andrianrandy9@gmail.com>
2023-02-08 23:58:17 +0700

    update

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -9,7 +9,7 @@ const categories = ref([]);
 async function getCategoriesData() {
   try {
     const response = await axios.get(
-      "https://zullkit-backend.buildwithangga.id/api/categories?show_product=1"
+      "https://zullkit-backend.buildwithangga.id/api/categories?show_product=1&limit=12"
     );
     categories.value = response.data.data.data;
   } catch (error) {
